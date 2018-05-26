7519f77a76346868a5c0a1fef617f08b07471962
Konstantin Vakula <v_kostya94@mail.ru>
2018-05-26 13:14:39 +0700

    [develop] maven example

---
+++ Example.java
@@ -0,0 +1,26 @@
+package com.lesson.example;
+
+/**
+ * Это пример простой программы на Java.
+ * Присвойте файлу с исходным кодом имя Example.java
+ */
+class Example {
+    public static void main(String args[]) {
+        int var;
+        double x;
+
+        var = 10;
+        x = 10.0;
+
+        System.out.println("Начальное значение переменной var " + var);
+        System.out.println("Начальное значение переменной x "+ x);
+        System.out.println();
+
+        var = var /4;
+        x = x/4;
+
+        System.out.println("Значение переменной var после деления " + var);
+        System.out.println("Значение переменной x после деления " + x);
+
+    }
+}
